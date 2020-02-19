11b564ccf9b2cf504c7986cb1c700bcab679df7f
Soler索莱尔HO <25105794+SolerHo@users.noreply.github.com>
2020-02-19 19:11:35 +0800

    11 基本内建类型

---
+++ Volume01/Chapter03/11.基本内建类型.cpp
@@ -0,0 +1,43 @@
+/**
+ * Date : 2020 - 02 - 18
+ * Author : Soler HO
+ * 
+ * Book :Thinking in C++
+ * 
+ * description :基本的内建数据类型
+ * 
+ * 数据类型（data type）定义使用存储空间（内存）的方式。
+ * 
+ * 数据类型可以是内部的或者是抽象的。
+ * 
+ * 内建数据类型是编译器本来能理解的数据类型，直接与编译器关联。
+ * 
+ * 用户定义的数据类型是创建的类型，作为一个类。一般成为抽象数据类型。
+ * 
+ * 四种基本数据类型
+ * 
+ * char —— 存储字符，存储容量：8位（一个字节）
+ * int —— 存储整数值，存储空间：2个字节
+ * 
+ * float 和 double  —— 存储浮点数，一般使用IEEE的浮点式，float —— 单精度，double —— 双精度
+ * 
+ * 如果要查看每一种数据类型的最大数值和最小数值的存储空间，可以使用系统头文件limits.h和cfloat.h。
+ * 
+ * 
+ */
+
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    char name;
+    int age;
+    float fiber;
+    double hight;
+}
+
+
+
